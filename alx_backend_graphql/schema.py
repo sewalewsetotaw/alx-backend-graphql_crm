6f220ff2215fb51c72e3eb6aa31d83edb2263d31
sewalewsetotaw <sewalewsetotaw2023@gmail.com>
2025-08-17 20:29:04 +0300

    final commit

--- alx_backend_graphql/schema.py
+++ alx_backend_graphql/schema.py
@@ -1,10 +1,7 @@
 import graphene
-from crm.schema import Query as CRMQuery, Mutation as CRMMutation
 
-class Query(CRMQuery, graphene.ObjectType):
-    pass
+class Query(graphene.ObjectType):
+    hello = graphene.String(default_value="Hello, GraphQL!")
 
-class Mutation(CRMMutation, graphene.ObjectType):
-    pass
+schema = graphene.Schema(query=Query)
 
-schema = graphene.Schema(query=Query, mutation=Mutation)
--- alx_backend_graphql/settings.py
+++ alx_backend_graphql/settings.py
@@ -124,3 +124,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+GRAPHENE = {
+    "SCHEMA": "alx_backend_graphql_crm.schema.schema"  # we’ll create this file next
+}
